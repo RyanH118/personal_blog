19860435f59ac5ecb5bf9f4673060ffb1286512b
ryanh <ryanhinson118@mail.com>
2024-04-09 19:25:33 -0400

    Added the foreach loop that will allow me to display the posts onto blog.html and comments to help me better understand what is happening.

---
+++ assets/js/blog.js
@@ -0,0 +1,16 @@
+// The parse is making the previous string back into a object and the OR operator with [] is for when theres nothing in the array.
+const blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+// this is calling on the div with the id so we can post the blog on the page.
+const blogPostList = document.getElementById('blogContainer');
+
+// This loop is what is reading over the array 
+blogPosts.forEach(post => {
+    // newPost is creating a new div for each new post.
+    const newPost = document.createElement('div');
+    newPost.innerHTML = `
+    <h2>${post.title}</h2>
+    <p>${post.content}</p>
+    <p>Author: ${post.username}</p>
+  `;
+    blogPostList.appendChild(newPost);
+});
--- assets/js/form.js
+++ assets/js/form.js
@@ -2,26 +2,30 @@ const form = document.getElementById("blogForm");
 
 // This event listener is waiting for the submit button to be clicked.
 form.addEventListener("submit", function (event) {
-    
-    //This array gets the content from the form and is for localStorage to send it all together.
-    const formData = {
-        username: document.getElementById("username").value,
-        title: document.getElementById("title").value,
-        content: document.getElementById("content").value
-    };
-    
+    //preventDefault will stop the page from going to the next page if nothing is filled out. 
+    event.preventDefault();
+        
     //This if is for when one of the textboxes is empty it pops up a message telling you to fill it out.
-    if (username === "" || title === "" || content === "") {
-        //preventDefault will stop the page from going to the next page if nothing is filled out. 
-        event.preventDefault();
+    if (!username === "" || !title === "" || !content === "") {
         document.getElementById("errorMessage").textContent = 'Please fill out the whole form!';
         //This setInterval makes the text disappear after 3 seconds.
         setInterval(() => {
             document.getElementById("errorMessage").textContent = '';
         }, 3000);
-        //When the whole form is filled out this sends it to localStorage.
-    } else {
-        localStorage.setItem("formData", JSON.stringify(formData));
     }
+    
+    //This array gets the content from the form and is for localStorage to send it all together.
+    const blogPost = {
+        username: document.getElementById("username").value,
+        title: document.getElementById("title").value,
+        content: document.getElementById("content").value
+    };
+    let blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+    // this push is adding a new post to the end of the array.
+    blogPosts.push(blogPost);
+    // stringify is making the objects into a string to send to localStorage. 
+    localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
 
+    // This takes you to the next page.
+    window.location.href = 'blog.html';
 });
