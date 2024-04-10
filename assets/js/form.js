b05b2110f34a242ece4601137bc6f7722bbff596
ryanh <ryanhinson118@mail.com>
2024-04-09 20:01:38 -0400

    Added a ifelse loop for when the form isn't fully filled out.

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,30 +2,34 @@ const form = document.getElementById("blogForm");
 
 // This event listener is waiting for the submit button to be clicked.
 form.addEventListener("submit", function (event) {
-    //preventDefault will stop the page from going to the next page if nothing is filled out. 
-    event.preventDefault();
-        
+    
+    const username = document.getElementById('username').value;
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+    
     //This if is for when one of the textboxes is empty it pops up a message telling you to fill it out.
-    if (!username === "" || !title === "" || !content === "") {
+    if (username === "" || title === "" || content === "") {
+        //preventDefault will stop the page from going to the next page if nothing is filled out. 
+        event.preventDefault();
         document.getElementById("errorMessage").textContent = 'Please fill out the whole form!';
         //This setInterval makes the text disappear after 3 seconds.
         setInterval(() => {
             document.getElementById("errorMessage").textContent = '';
         }, 3000);
+    } else {
+
+        //This array gets the content from the form and is for localStorage to send it all together.
+        const blogPost = {
+            username: document.getElementById("username").value,
+            title: document.getElementById("title").value,
+            content: document.getElementById("content").value
+        };
+        let blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+        // this push is adding a new post to the end of the array.
+        blogPosts.push(blogPost);
+        // stringify is making the objects into a string to send to localStorage. 
+        localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
+    
     }
     
-    //This array gets the content from the form and is for localStorage to send it all together.
-    const blogPost = {
-        username: document.getElementById("username").value,
-        title: document.getElementById("title").value,
-        content: document.getElementById("content").value
-    };
-    let blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
-    // this push is adding a new post to the end of the array.
-    blogPosts.push(blogPost);
-    // stringify is making the objects into a string to send to localStorage. 
-    localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
-
-    // This takes you to the next page.
-    window.location.href = 'blog.html';
 });
