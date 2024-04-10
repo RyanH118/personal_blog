f09a6e9a2dc75a29ed57f2abbaa24b50cb8a3fa3
ryanh <ryanhinson118@mail.com>
2024-04-10 16:34:30 -0400

    Some comments for future me.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,7 +7,9 @@ function goBack() {
 const themeToggle = document.getElementById('theme');
 const body = document.body;
 
+// event listener is waiting for a click on the "Click me!" button.
 themeToggle.addEventListener('click', () => {
+  // This if else is checking for the class to see if it's there and if it needs to be added when clicked.
   body.classList.toggle('light-theme');
   if (body.classList.contains('light-theme')) {
     localStorage.setItem('theme', 'light');
