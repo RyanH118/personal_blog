a23e40ed16d73a751293586f662131f1c50ff5fc
ryanh <ryanhinson118@mail.com>
2024-04-09 23:53:53 -0400

    Added dark mode and a button for light mode.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,22 @@
+// this function is what makes the page go back.
 function goBack() {
     window.history.back();
-    }
+}
 
-    
+
+const themeToggle = document.getElementById('theme');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+  if (body.classList.contains('dark-theme')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
+});
+
+const storedTheme = localStorage.getItem('theme');
+if (storedTheme === 'dark') {
+  body.classList.add('dark-theme');
+}
